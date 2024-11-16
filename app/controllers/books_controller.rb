61f27bfa628ea85e8d7595d6b1c494e7cbd4fa28
k3310 <187008159+k3310@users.noreply.github.com>
2024-11-16 10:51:10 +0000

    [Update] booのページと機能k フラッシュメッセージ, バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,44 @@
-class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
+class BooksController < ApplicationController 
 
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "Book was successfuly created"
+      flash[:notice] = "Book was successfully created"
       redirect_to book_path(@book.id)
     else
-      flash.now[:alert] = "error"
+      @books = Book.all.order(id: :asc)
       render :index
+    end
   end
 
   def index
-    @books = Book.all
+    @books = Book.all.order(id: :asc)
     @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfuly updated"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfuly destroyed"
+    redirect_to books_path
   end
 
   private
